d373dbc4da54709fe902372868b164d96e9ae9ce
Ansley Orrell <ansley.orrell@gmail.com>
2025-03-21 21:43:33 -0400

    final

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+﻿﻿using System;
 using System.Collections.Concurrent;
 using System.Diagnostics;
 using System.IO.Pipelines;
@@ -46,13 +46,67 @@ class FactorCounter
         }
     }
 
+class GeneratePrimeNumbers{
+    public List<BigInteger> generatePrimes(int bits, int count){
+
+        RandomBigIntegerGenerator generator = new RandomBigIntegerGenerator();
+
+        ParallelOptions parallelOptions = new ParallelOptions { MaxDegreeOfParallelism = Environment.ProcessorCount*2 };
+
+        ConcurrentBag<BigInteger> primeNumbers = new ConcurrentBag<BigInteger>();
+
+        Parallel.For(1, int.MaxValue, parallelOptions, (i, pls) =>
+        {
+            BigInteger primeCandidate = generator.GenerateRandomBigInteger(bits);
+            if (primeCandidate.IsProbablyPrime())
+            {
+                primeNumbers.Add(primeCandidate);
+            }
+            if(primeNumbers.Count >= count)
+            {
+                pls.Stop();
+            }
+        });
+
+        List<BigInteger> result = primeNumbers.Take(count).ToList();
+
+        return result;
+    }
+}
+
+class OddNumberGenerator
+{
+    public void GenerateOddNumbers(int bits, int count)
+    {
+        for (int i = 0; i < count; i++)
+        {
+            FactorCounter factorCounter = new FactorCounter();
+
+            RandomBigIntegerGenerator generator = new RandomBigIntegerGenerator();
+
+            BigInteger randomNumber = generator.GenerateRandomBigInteger(bits);
+            
+            while (randomNumber % 2 == 0)
+            {
+                randomNumber = generator.GenerateRandomBigInteger(bits);
+            }
+
+            Console.WriteLine($"{i + 1}: {randomNumber}");
+            int factorCount = factorCounter.CountFactors(randomNumber);
+            Console.WriteLine($"Number of factors: {factorCount}");
+            if (i < count - 1)
+            {
+                Console.WriteLine();
+            }
+        }
+    }
+}
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine(Environment.ProcessorCount);
-        
+
         if (args.Length < 2 || args.Length > 3)
         {
             Console.WriteLine("Usage: dotnet run <bits> <option> <count>");
@@ -74,62 +128,31 @@ class Program
         RandomBigIntegerGenerator generator = new RandomBigIntegerGenerator();
 
         Console.WriteLine($"Bitlength: {bits} bits");
-        
-        for (int i = 0; i < count; i++)
+
+        if (option == "prime")
         {
-            BigInteger randomNumber = generator.GenerateRandomBigInteger(bits);
-
-            if (option == "prime")
+            GeneratePrimeNumbers primeGenerator = new GeneratePrimeNumbers();
+            List<BigInteger> primeNumbers = primeGenerator.generatePrimes(bits, count);
+            for (int i = 0; i < primeNumbers.Count; i++)
             {
-                ParallelOptions parallelOptions = new ParallelOptions { MaxDegreeOfParallelism = Environment.ProcessorCount*2 };
-
-                ConcurrentBag<BigInteger> primeNumbers = new ConcurrentBag<BigInteger>();
-                int counter = 0;
-
-                Parallel.For(1, int.MaxValue, parallelOptions, (i, pls) =>
+                Console.WriteLine($"{i + 1}: {primeNumbers[i]}");
+                if (i < primeNumbers.Count - 1)
                 {
-                    BigInteger primeCandidate = generator.GenerateRandomBigInteger(bits);
-                    counter++;
-                    if (primeCandidate.IsProbablyPrime())
-                    {
-                        primeNumbers.Add(primeCandidate);
-                    }
-                    if(primeNumbers.Count == count)
-                    {
-                        pls.Stop();
-                    }
-                    if (counter % 1000 == 0)
-                    {
-                        Console.WriteLine($"Checked {counter} numbers.");
-                    }
-                });
-
-                Console.WriteLine($"Prime Numbers Found: {string.Join(", ", primeNumbers)}");
-
-                Console.WriteLine($"{i + 1}: {randomNumber}");
-                Console.WriteLine($"Number of attempts to find prime: {counter}");
-                Console.WriteLine($"Time Per Iteration: {stopwatch.Elapsed.Milliseconds/(counter*1.0)}ms");
-            }
-            else if (option == "odd")
-            {
-                FactorCounter factorCounter = new FactorCounter();
-
-                while (randomNumber % 2 == 0)
-                {
-                    randomNumber = generator.GenerateRandomBigInteger(bits);
+                    Console.WriteLine();
                 }
-
-                Console.WriteLine($"{i + 1}: {randomNumber}");
-                int factorCount = factorCounter.CountFactors(randomNumber);
-                Console.WriteLine($"Number of factors: {factorCount}\n");
-            }
-            else
-            {
-                Console.WriteLine("Error: option must be 'prime' or 'odd'.");
-                return;
             }
         }
-
+        else if (option == "odd")
+        {
+            OddNumberGenerator oddGenerator = new OddNumberGenerator();
+            oddGenerator.GenerateOddNumbers(bits, count);
+        }
+        else
+        {
+            Console.WriteLine("Error: option must be 'prime' or 'odd'.");
+            return;
+        }
+    
         stopwatch.Stop();
         Console.WriteLine($"Time to Generate: {stopwatch.Elapsed}");
     }
--- obj/Debug/net9.0/NumGen.AssemblyInfo.cs
+++ obj/Debug/net9.0/NumGen.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NumGen")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f6bc380eff23b14f8a3e38785541d93267696e35")]
 [assembly: System.Reflection.AssemblyProductAttribute("NumGen")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NumGen")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
